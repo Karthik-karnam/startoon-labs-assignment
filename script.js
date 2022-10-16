c99280a0cdd31bc0e32c052d11c249d14bf8ff32
Karthik <90787191+Karthik-karnam@users.noreply.github.com>
2022-10-16 20:13:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ let finalValue = 0;
 let max = 0;
 
 function changeWidth(){
-    progress.style.width=`${(finalValue/max)*100}%`;
+    if(finalValue==null){
+        progress.style.width=0;
+        alert("Please enter a value");
+    }
+    if(finalValue<=max){
+        progress.style.width=`${(finalValue/max)*100}%`;
+    } 
+    else if(finalValue>max){
+        progress.style.width=0;
+        alert("Please enter value less than max value");
+    }
+    else{
+        progress.style.width=0;
+        alert('Enter a value');
+    }
 }
 input.addEventListener("keyup",function(){
     finalValue = parseInt(input.value, 10);
