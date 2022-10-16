e247b79d06f1f9969a9b2ca299e89d8eba5e5969
Karthik <90787191+Karthik-karnam@users.noreply.github.com>
2022-10-16 18:57:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const progress= document.querySelector(".progress-done");
+const input = document.querySelector(".input");
+const maxInput= document.querySelector(".maxInput");
+let finalValue = 0;
+let max = 0;
+
+function changeWidth(){
+    progress.style.width=`${(finalValue/max)*100}%`;
+}
+input.addEventListener("keyup",function(){
+    finalValue = parseInt(input.value, 10);
+    changeWidth();
+});
+input.addEventListener("keyup",function(){
+    max = parseInt(maxInput.value, 10);
+    changeWidth();
+});
